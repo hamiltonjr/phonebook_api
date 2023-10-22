4455ee8b9d3c877c1d42cda59ea2b90bcc10b193
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2023-10-22 03:03:15 -0300

    Included model class

---
+++ src/main/java/com/api/phonebook/model/BookModel.java
@@ -0,0 +1,45 @@
+package com.api.phonebook.model;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="tb_book")
+public class BookModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 60)
+    private String name;
+
+    @Column(length = 60)
+    private String email;
+
+    @Column(length = 30)
+    private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+}
